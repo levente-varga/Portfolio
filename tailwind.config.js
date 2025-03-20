29c08e1d57b8701901855f143b7de5bef5ac995b
Levente <v.levente1999@gmail.com>
2025-03-20 18:26:42 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: "#366B45"
+
             },
             fontFamily: {
                 sans: ["Inter", "sans-serif"],
