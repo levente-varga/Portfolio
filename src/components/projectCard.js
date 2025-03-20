aebb5f290b81832ca1fb8ec03517b2820b682354
Levente <v.levente1999@gmail.com>
2025-03-21 00:42:01 +0100

    Add ProjectCard component

---
+++ src/components/projectCard.js
@@ -0,0 +1,39 @@
+class ProjectCard extends HTMLElement {
+    static get observedAttributes() {
+        return ["title", "description", "bg", "tags"];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+
+    render() {
+        const title = this.getAttribute("title") || "Title";
+        const description = this.getAttribute("description") || "Description";
+        const bg = this.getAttribute("bg") || "";
+        const tags = this.getAttribute("tags") ? JSON.parse(this.getAttribute("tags")) : {};
+
+        this.innerHTML = `
+            <div class="relative rounded-2xl m-4 sm:w-10 md:w-20 lg:30 xl:w-40 2xl:w-50 w-100 ring-1 ring-white/5 bg-foreground shadow-xl">
+                <img class="rounded-t-2xl aspect-[calc(5/3)]" src="${bg}" alt="${title}"/>
+                
+                <div class="p-6">
+                    <div class="text-xl text-textLight font-bold">${title}</div>
+                    <div class="text-sm text-text font-light my-2">${description}</div>
+                    <ul class="flex text-text items-center">
+                        ${Object.entries(tags).map(([tag, color]) => `
+                            <li class="relative w-3 h-3 rounded-2xl bg-${color} inset-ring-1 inset-ring-white/20"></li>
+                            <li class="pr-4 pl-1 text-sm font-light">${tag}</li>
+                        `).join("")}
+                    </ul>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define("project-card", ProjectCard);
