cbd2c0f51f91cad45226c270b6a7967750b08f9b
Levente <v.levente1999@gmail.com>
2025-03-21 16:53:06 +0100

    Implement references

--- main.js
+++ main.js
@@ -48,3 +48,19 @@ education.forEach((education, index) => {
     timelineRow.setAttribute('last', last ? '' : null);
     educationContainer.appendChild(timelineRow);
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 80; // Adjust based on your header height
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - offset,
+                behavior: "smooth"
+            });
+        }
+    });
+});
