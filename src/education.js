dea08d80d3e7e31fe07f815cce967f20e8f9a79e
Levente <v.levente1999@gmail.com>
2025-03-21 16:49:05 +0100

    Add more details

--- src/education.js
+++ src/education.js
@@ -3,14 +3,14 @@ export const education = [
         title: "Game Design and Technology MSc",
         subtitle: "University of Gothenburg",
         time: "Aug 2022 - Jun 2025",
-        description: "Experience in game engines, advanced computer graphics (OpenGl, DirectX), game design, and game development (Godot).",
+        description: "Experience in game engines, advanced computer graphics (OpenGl, DirectX), game design, game development (Godot) and web development (React).",
         logo: "/images/education/gothenburg.jpg"
     },
     {
         title: "Computer Science and Engineering BSc",
         subtitle: "Budapest University of Technology and Engineering",
         time: "Sep 2018 - Jan 2022",
-        description: "Experience with computer graphics, object-oriented programming (C++, Java, C#), mobile development (Flutter, Android), and game development (Unity).",
+        description: "Experience with computer graphics, object-oriented programming (C++, C#, Kotlin, Dart, Java), design patterns, clean code principles, mobile development (Flutter, Android), web-development (JavaScript) and game development (Unity).",
         logo: "/images/education/budapest.jpg",
     }
 ];
