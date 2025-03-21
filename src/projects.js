171f08a88cffcf7616abb12dedf5d63906ea447d
Levente <v.levente1999@gmail.com>
2025-03-21 22:38:57 +0100

    Move cardium project page to root

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ export const projects = [
         image: "/images/projects/cardium.png",
         tags: '{"C#": "csharp", "Godot": "godot"}',
         year: "2025",
-        url: "/projects/cardium.html"
+        url: "cardium.html"
     },
     {
         title: "Ecometer Desktop",
