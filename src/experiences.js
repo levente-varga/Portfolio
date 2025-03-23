9c91e5b29519499914a0000de4d1027c232ca2a0
Levente <v.levente1999@gmail.com>
2025-03-23 14:01:46 +0100

    Add more details

--- src/experiences.js
+++ src/experiences.js
@@ -3,21 +3,21 @@ export const experiences = [
         title: "Flutter Developer",
         subtitle: "Ecometer Kft.",
         time: "May 2024 - Present",
-        description: "Cross platform mobile and desktop development in Flutter (Dart) using GitHub, and UI design in Figma.",
+        description: "Cross platform mobile and desktop development in Flutter (Dart) using GitHub as lead frontend developer. UI design in Figma.",
         logo: "/images/experience/ecometer.png",
     },
     {
         title: "R&D Intern",
         subtitle: "Ericsson AB",
         time: "Jun 2023 - Dec 2023",
-        description: "Unit-test generator plugin development for Clang in modern C++ using GitLab.",
+        description: "Unit-test generator plugin development for Clang in modern C++ using GitLab. Weekly standups, and a project presentation at the end of internship.",
         logo: "/images/experience/ericsson.png",
     },
     {
         title: "Mobile Software Developer",
         subtitle: "Arteries Studio Kft.",
-        time: "May 2024 - Present",
-        description: "Cross-platform mobile development in Flutter using Github as part of an agile team.",
+        time: "Apr 2022 - Aug 2022",
+        description: "Cross-platform mobile development in Flutter using GitHub as part of an agile team. Participated in standups, worked with the UI designer, tester and backend developer.",
         logo: "/images/experience/arteries.png",
     }
 ];
