f31363558d36ec85a168395c282a8a2dcf6a429a
Levente <v.levente1999@gmail.com>
2025-03-24 00:13:03 +0100

    Rewrite awards

--- src/awards.js
+++ src/awards.js
@@ -1,16 +1,16 @@
 export const awards = [
     {
         title: "Csizmás Lajos Scholarship",
-        subtitle: "Óbudai Árpád Secondary Grammaer School.",
+        subtitle: "Óbudai Árpád Secondary Grammaer School",
         time: "May 2018",
-        description: "Granted for my achievements in informatics, robotics and mathematics national competitions and academic performance.",
+        description: "Granted for my achievements in national informatics competitions (two-time finalist), robotics team competitions (numerous awards), mathematics team competitions (two-time second place) and academic performance.",
         logo: "/images/awards/arpad.png",
     },
     {
         title: "Special Award for Robot Design",
         subtitle: "World Robot Olympiad",
         time: "May 2016",
-        description: "Granted for the robot designed by me. The robot was able to navigae on a table and finish objectives. It featured a unique design among the other contestants",
+        description: "Granted for the robot designed by me. It featured a unique design among the other contestants.",
         logo: "/images/awards/wro.png",
     }
 ];
