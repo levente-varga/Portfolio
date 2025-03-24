37fdb6ed2bdcb842e136ed6cad2bfc667a74bb79
Levente <v.levente1999@gmail.com>
2025-03-24 01:44:32 +0100

    Fix typo

--- src/awards.js
+++ src/awards.js
@@ -1,7 +1,7 @@
 export const awards = [
     {
         title: "Csizmás Lajos Scholarship",
-        subtitle: "Óbudai Árpád Secondary Grammaer School",
+        subtitle: "Óbudai Árpád Secondary Grammar School",
         time: "May 2018",
         description: "Granted for my achievements in national informatics competitions (two-time finalist), robotics team competitions (numerous awards), mathematics team competitions (two-time second place) and academic performance.",
         logo: "/images/awards/arpad.png",
