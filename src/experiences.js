888b25d929b381b34968e675e2e4e45c67988d8f
Levente <v.levente1999@gmail.com>
2025-03-29 19:19:18 +0100

    Revise experience descriptions

--- src/experiences.js
+++ src/experiences.js
@@ -5,7 +5,7 @@ export const experiences = [
         companyUrl: "https://ecometer.hu/",
         logo: "/images/experience/ecometer.png",
         time: "May 2024 - Present",
-        description: "Cross platform mobile and desktop development in Flutter (Dart) using GitHub as the lead frontend developer. UI design in Figma.",
+        description: "Cross platform mobile and desktop development in Flutter (Dart) using GitHub. Communication with the server through RESTful APIs and with taximeter units via Bluetooth. Full responsibility for frontend tasks. Designed both apps in Figma.",
     },
     {
         name: "R&D Intern",
@@ -13,7 +13,7 @@ export const experiences = [
         companyUrl: "https://www.ericsson.com/",
         logo: "/images/experience/ericsson.png",
         time: "Jun 2023 - Dec 2023",
-        description: "Unit-test generator plugin development for Clang in modern C++ using GitLab. Weekly standups, and a project presentation at the end of internship.",
+        description: "Unit-test generator plugin development for Clang in modern C++ using GitLab, achieving up to 80% code coverage. Weekly standups, and a project presentation at the end of the internship.",
     },
     {
         name: "Mobile Software Developer",
@@ -21,6 +21,6 @@ export const experiences = [
         companyUrl: "https://arteries.hu/en/",
         logo: "/images/experience/arteries.png",
         time: "Apr 2022 - Aug 2022",
-        description: "Cross-platform mobile development in Flutter using GitHub as part of an agile team. Participated in standups, worked with the UI designer, tester and backend developer.",
+        description: "Cross-platform mobile development in Flutter using GitHub as part of an agile team. Implemented push notifications with Firebase, and social logins for Google and Apple accounts. Participated in standups, worked with the UI designer, tester and backend developer.",
     }
 ];
